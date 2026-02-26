48e9f4f6f1a337c8497eba534f05346418a8a78f
LoKiiZoshi <lokendarjoshi384@gmail.com>
2026-02-26 23:01:16 +0545

    app config

--- skills/apps.py
+++ skills/apps.py
@@ -3,3 +3,13 @@ from django.apps import AppConfig
 
 class SkillsConfig(AppConfig):
     name = 'skills'
+
+
+default_auto_field = 'django.db.model.BigAutoField'
+name = 'skills'
+verbose_name = 'Skill Exchanges & Sessions'
+ 
+def ready(self):
+    # Import signals here if needed
+    import skills.signals
+    
