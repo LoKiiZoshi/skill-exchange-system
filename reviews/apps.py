e7dec70016121e9cdd83d44bb4f2b7c9ec0b5e93
LoKiiZoshi <lokendarjoshi384@gmail.com>
2026-07-06 13:45:20 +0545

    app config

--- reviews/apps.py
+++ reviews/apps.py
@@ -2,4 +2,8 @@ from django.apps import AppConfig
 
 
 class ReviewsConfig(AppConfig):
+    
+    defult_auto_fields = 'django.db.models.BigAutoField'
+    
     name = 'reviews'
+    label = 'reviews'
